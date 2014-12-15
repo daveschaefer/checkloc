dc8df0528e066192603b712aa4faf85f8d5badc9
Dave Schaefer <dave.schaefer@gmail.com>
2014-12-14 21:29:46 -0700

    checkloc - Remove list of test cases
    Let's document things in only one place;
    for now, the README

--- checkloc.py
+++ checkloc.py
@@ -19,36 +19,6 @@ Validate Mozilla-style localization files to make sure all localizations
 have the same strings in the same places.
 """
 
-"""
-Current test cases for each localization:
-	- Loc has no extra files
-	- Loc has no missing files
-	- Loc has at least one key
-	- Loc has no extra keys
-	- Loc has no missing keys
-	- Loc has no duplicate keys defined in the same .properties file
-	(the same key name defined in different files is okay -
-	 presumably they will be loaded and used in different stringbundles)
-	- Key values are not empty
-	- Key names contain no invalid characters, including "!@#$%^&*<>[](){} ?
-	- DTD values contain no invalid characters, including "%<
-	- DTD comments contain no double hyphens '--'
-	- .properties values are valid, meaning either:
-		1. no % on a line
-		2. double %% to escape and print a regular %
-		3. %S or %n$S , where n is a number, for formatted string replacement.
-	- .properties values use the same count and type of string substitutions across languages
-	- No files contain the Byte Order Marker (BOM)
-
-Unimplemented:
-	- Test that loc has no duplicate DTD entities defined in the same file
-	(currently an lxml limitation; lxml removes duplicate entities when parsing.
-	but so does firefox, so the built plugin will still be valid.
-	this test would simply be a warning for builders, as it's probably not what	they intended).
-
-Feel free to add more tests!
-"""
-
 import argparse
 import codecs
 import logging
