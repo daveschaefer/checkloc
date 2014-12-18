cd9041eea9ccd9ea23d3e3ecc621f8e5771fd6dd
Dave Schaefer <dave.schaefer@gmail.com>
2014-12-17 21:49:40 -0700

    checkloc - Allow additional characters in .properties keys
    Based on testing and reading through specs

--- checkloc.py
+++ checkloc.py
@@ -62,7 +62,10 @@ BASE_LOC = 'en-US'
 # Assumptions: both comments and entries exist only on a single line.
 PROP_COMMENT = re.compile('[#!]+[^\n\r\f]*[\n\r\f]', re.DOTALL)
 PROP_SEP = re.compile('[\n\r\f]')
-PROP_LINE = re.compile('([A-Za-z0-9]+)=([^\n\r\f]*)')
+# almost any character is a valid .properties key
+# except : and = , which note the transition to a value
+# note that \\\\ is used to specify a single \ is allowed in keys
+PROP_LINE = re.compile('([A-Za-z0-9_.\-+\\\\{}\[\]!@$%^&*()/<>,?;\'"`~|]+)=([^\n\r\f]*)')
 
 DTD_PARSE_ERROR = re.compile('([^:]*):([^:]*):([^:]*):([^:]*):([^:]*):([^:]*):(.*)', re.DOTALL)
 
