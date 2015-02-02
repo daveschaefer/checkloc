39317a09ef544f84d34be376a3a2db43e9a6f0db
Dave Schaefer <dave.schaefer@gmail.com>
2015-02-01 19:46:11 -0700

    checkloc, tests - Begin converstion to new interface
    Main behaviour will be: point script to the manifest files

--- checkloc.py
+++ checkloc.py
@@ -43,12 +43,12 @@ import localecodes
 # MAJOR version when you make backwards-incompatible changes,
 # MINOR version when you add functionality in a backwards-compatible manner
 # PATCH version when you make backwards-compatible bug fixes.
-VERSION = "1.3.1"
+VERSION = "2.0a"
 
 # the en-US translation will have all files and strings created. Use it as the base.
 BASE_LOC = 'en-US'
 
-MANIFEST_LOCALE_LINE = re.compile('^\s*locale\s+\S+\s+(\S+)')
+MANIFEST_LOCALE_LINE = re.compile('^\s*locale\s+\S+\s+(\S+)\s+(\S+)')
 
 any_errors = False
 
@@ -127,7 +127,7 @@ class LocalizationLanguage:
 		# all string substitutions found in .properties files
 		self.subs = {}
 
-		self.loc_dir = os.path.join(localization_base_dir, language)
+		self.loc_dir = localization_base_dir
 		self.name = language
 
 		self.parsing_errors = False
@@ -336,9 +336,14 @@ def _validate_manifests(loc_dir, langs):
 	"""
 	Validate localization contents of the Mozilla extension information files:
 	chrome.manifest and install.rdf.
+	Return an array of locale base folders.
 	"""
 
-	plugin_dir = os.path.abspath(os.path.join(loc_dir, '../..'))
+	loc_folders = {}
+
+	# TODO: have to make sure it fails for the correct reason
+	# TODO: update the --no-manifest switch text here
+	plugin_dir = loc_dir
 	if not (os.path.exists(plugin_dir) and os.path.isdir(plugin_dir)):
 		_log_error("Main plugin directory {0} does not exist; cannot validate chrome.manifest. "
 			"If you wish to skip validation of chrome.manifest please specify the "
@@ -372,11 +377,17 @@ def _validate_manifests(loc_dir, langs):
 			match = MANIFEST_LOCALE_LINE.match(line)
 			if match:
 				locale = match.groups(1)[0]
-				if locale not in manifest_locales:
-					manifest_locales[locale] = i
+				locale_subdir = match.group(2)
+				# go one dir up to get the main locale directory
+				locale_absdir = os.path.abspath(os.path.join(loc_dir, locale_subdir, '..'))
+				if (locale_absdir not in loc_folders):
+					# TODO: save these so install.rdf can find its contents
+					loc_folders[locale_absdir] = True
+				if locale_subdir not in manifest_locales:
+					manifest_locales[locale_subdir] = i
 				else:
 					_log_error("Locale '{0}' is defined more than once inside chrome.manifest. "
-						"Each locale should only be defined once.".format(locale))
+						"Each locale should only be defined once.".format(locale_subdir))
 			i += 1
 
 
@@ -444,9 +455,11 @@ def _validate_manifests(loc_dir, langs):
 			warnings.warn("Locale folder '{0}' exists in {1}, but no corresponding entry "
 				"exists in install.rdf.".format(lang, loc_dir))
 
+	return loc_folders.keys()
 
 
-def validate_loc_files(loc_dir, parse_manifests=True):
+
+def validate_loc_files(manifest_dir, locales_only=False):
 	"""
 	Validate localization contents inside the given base directory.
 	Return True if there were any errors and False otherwise.
@@ -458,35 +471,46 @@ def validate_loc_files(loc_dir, parse_manifests=True):
 
 	print "Starting Localization tests..."
 
-	loc_dir = os.path.abspath(loc_dir)
-	if not (os.path.exists(loc_dir)):
-		_log_error("The localization directory {0} does not exist!".format(loc_dir))
+	manifest_dir = os.path.abspath(manifest_dir)
+	if not (os.path.exists(manifest_dir)):
+		_log_error("The localization directory {0} does not exist!".format(manifest_dir))
 		return True
-	logging.info("Loc directory {0} exists.".format(loc_dir))
+	logging.info("Loc directory {0} exists.".format(manifest_dir))
 
-	if not (os.path.isdir(loc_dir)):
-		_log_error("{0} is not a directory!".format(loc_dir))
+	if not (os.path.isdir(manifest_dir)):
+		_log_error("{0} is not a directory!".format(manifest_dir))
 		return True
-	logging.info("{0} is a directory.".format(loc_dir))
+	logging.info("{0} is a directory.".format(manifest_dir))
 
-	for (root, dirs, files) in os.walk(loc_dir):
-		for dir in dirs:
-			langs[dir] = True
+
+	loc_dirs = []
+	if (locales_only):
+		loc_dirs.append(manifest_dir) # script should be pointed to main locale folder instead
+	else:
+		dirs = _validate_manifests(manifest_dir, langs)
+		if dirs:
+			loc_dirs.extend(dirs)
+
+	if not loc_dirs:
+		_log_error("No localization directories found in {0}".format(manifest_dir))
+		return True
+
+	for ld in loc_dirs:
+		for (root, dirs, files) in os.walk(ld):
+			for d in dirs:
+				langs[d] = os.path.join(ld, d)
 
 	if (len(langs) < 1):
-		_log_error("Did not find any language folders inside {0}!".format(loc_dir))
+		_log_error("Did not find any language folders inside {0}!".format(loc_dirs))
 		return True
 	print "Found {0} languages: {1}.".format(len(langs), langs.keys())
 
 	if BASE_LOC not in langs:
 		_log_error("Base language folder '{0}' was not found in {1}".format(\
-			BASE_LOC, loc_dir))
+			BASE_LOC, loc_dirs))
 		return True
 
-	if (parse_manifests):
-		_validate_manifests(loc_dir, langs)
-
-	baseline = LocalizationLanguage(loc_dir, BASE_LOC)
+	baseline = LocalizationLanguage(langs[BASE_LOC], BASE_LOC)
 	parse_errors = baseline.get_loc_keys()
 	any_errors = any_errors or parse_errors
 
@@ -503,7 +527,7 @@ def validate_loc_files(loc_dir, parse_manifests=True):
 	del langs[BASE_LOC] # don't test the baseline localization against itself
 
 	for lang in langs:
-		loc = LocalizationLanguage(loc_dir, lang)
+		loc = LocalizationLanguage(langs[lang], lang)
 		parse_errors = loc.get_loc_keys()
 		any_errors = any_errors or parse_errors
 
@@ -545,16 +569,18 @@ def validate_loc_files(loc_dir, parse_manifests=True):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description=__doc__)
-	parser.add_argument('loc_dir',
-			help="Directory where Mozilla-style localization files are located.")
+	parser.add_argument('manifest_dir',
+			help="Directory where chrome.manifest file is located.")
 	verbosity_group = parser.add_mutually_exclusive_group()
 	verbosity_group.add_argument('--verbose', '-v', default=False, action='store_true',
 			help="Verbose mode. Print more info about files and tests.")
 	verbosity_group.add_argument('--quiet', '-q', default=False, action='store_true',
 			help="Quiet mode. Don't print much, not even error info.")
 
-	parser.add_argument('--no-manifest', '--nm', default=False, action='store_true',
+	parser.add_argument('--locales-only', '-l', default=False, action='store_true',
 			help="Do not attempt to parse or validate chrome.manifest or install.rdf. "
+				"Instead, point the script directly to your locale folder: "
+				"it will treat all subfolders as locales and parse them individually. "
 				"Mainly intended to allow easier unit-testing of checkloc itself; "
 				"you should usually *NOT* use this flag.")
 
@@ -572,11 +598,11 @@ if __name__ == '__main__':
 	logging.captureWarnings(True)
 	warnings.formatwarning=_format_warning
 
-	parse_manifests = True
-	if (args.no_manifest):
-		parse_manifests = False
+	locales_only = False
+	if (args.locales_only):
+		locales_only = True
 
-	errors = validate_loc_files(args.loc_dir, parse_manifests=parse_manifests)
+	errors = validate_loc_files(args.manifest_dir, locales_only=locales_only)
 	if (errors):
 		sys.exit(1)
 	else:
